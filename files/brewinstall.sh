07f735ae72305c4e6728eb540097782995aacdb5
joker <joker@jokers-MacBook-Pro.local>
2022-12-07 08:45:38 +0800

    typo fixed

--- files/brewinstall.sh
+++ files/brewinstall.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
-PACAGES="git wget tree mas appcleaner onyx cleanmymac bettertouchtool alfred blackhole-64ch sublime-text visual-studio-code typora google-chrome firefox microsoft-edge brave-browser opera tor-browser surge android-studio audacity baidunetdisk calibre dropbox keka microsoft-remote-desktop miniconda obs openaudible sketch skitch spotify thunder transmission vlc webcatalog wechat"
+PACKAGES="git wget tree mas appcleaner onyx cleanmymac bettertouchtool alfred blackhole-64ch sublime-text visual-studio-code typora google-chrome firefox microsoft-edge brave-browser opera tor-browser surge android-studio audacity baidunetdisk calibre dropbox keka microsoft-remote-desktop miniconda obs openaudible sketch skitch spotify thunder transmission vlc webcatalog wechat"
 
-for p in $PACAGES; do
+for p in $PACKAGES; do
 	if (echo $(brew list)  | fgrep -q $p); then
 		echo "$p has already installed."
 	elif (echo $(brew list --cask)  | fgrep -q $p); then
